016ea04d211cc37fcf5728bbd2eb6a6578be781a
anqi <18742024663@163.com>
2020-07-03 21:17:53 +0800

    deploy

---
+++ precache-manifest.fe244eb61ed41a0474e48da7408f4cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ce4d6fd3a1b49bbe4ccd4ef3cb69af",
+    "url": "/account_r_website/index.html"
+  },
+  {
+    "revision": "404ef63b34f3a9c30a36",
+    "url": "/account_r_website/static/css/main.08beb5c3.chunk.css"
+  },
+  {
+    "revision": "df1476bb6ab46fe0e76e",
+    "url": "/account_r_website/static/js/2.7a4c5a7f.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/account_r_website/static/js/2.7a4c5a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404ef63b34f3a9c30a36",
+    "url": "/account_r_website/static/js/main.720f4828.chunk.js"
+  },
+  {
+    "revision": "86191fda29f5ce0da37e",
+    "url": "/account_r_website/static/js/runtime-main.801b90d6.js"
+  }
+]);
